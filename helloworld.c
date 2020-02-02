77731fced2935529467d694849684b2e0ba46531
Steven Tran <60561178+stran1225@users.noreply.github.com>
2020-02-01 21:03:20 -0700

    Simple hello world code for C

---
+++ helloworld.c
@@ -0,0 +1,15 @@
+/*
+ *Steven Tran
+ *
+ *Simple C code for Hello World
+ *
+ */
+ 
+#include <stdio.h>
+
+int main() 
+{
+   // simple print statement of hello world.
+   printf("Hello World!");
+   return 0;
+}
